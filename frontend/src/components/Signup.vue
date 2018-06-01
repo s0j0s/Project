6ff987e240da86bc59f29908ddec62d67d1ae269
Jinwoo Bae <tkqlzz@gmail.com>
2018-06-02 03:15:05 +0900

    SignUp 컴포넌트및 gitignore 수정
    SignUp 회원가입 버튼 클릭후 처리작업 추가
    프론트에서 빌드된 파일들 git에서 무시하도록 추가 (backend/public 폴더)

--- frontend/src/components/Signup.vue
+++ frontend/src/components/Signup.vue
@@ -4,14 +4,14 @@
       <h2 class="card-title text-center" style="color:#113366;">회원가입</h2>
     </div>
     <div class="card-body">
-      <form class="form-signin">
+      <form class="form-signin" @submit.prevent="SignUp">
         <h5 class="text-left" style="font-size:1em;">회원정보를 등록하세요.</h5>
-        <input type="text" class="form-control" placeholder="아이디"><br/>
-        <input type="password" class="form-control" placeholder="비밀번호"><br />
+        <input v-model="userId" type="text" class="form-control" placeholder="아이디"><br/>
+        <input v-model="password" type="password" class="form-control" placeholder="비밀번호"><br />
         <input type="password" class="form-control" placeholder="비밀번호 확인"><br />
-        <input type="text" class="form-control" placeholder="이름"><br />
-        <input type="email" class="form-control" placeholder="이메일"><br />
-        <button class="btn btn-lg btn-primary btn-block" type="submit">회원가입</button>
+        <input v-model="name" type="text" class="form-control" placeholder="이름"><br />
+        <input v-model="email" type="email" class="form-control" placeholder="이메일"><br />
+        <button class="btn btn-lg btn-primary btn-block">회원가입</button>
       </form>
     </div>
   </div>
@@ -19,7 +19,33 @@
 
 <script>
 export default {
-  name: "Signup"
+  name: "Signup",
+  data: function () {
+    return {
+      userId: '',
+      password: '',
+      name: '',
+      email: ''
+    }
+  },
+  methods: {
+    SignUp () {
+      this.$http.post('/api/users', {
+        userId: this.userId,
+        passsword: this.password,
+        name: this.name,
+        email: this.email
+      }).then((res) => {
+        if (res.success) {
+          this.$router.push('Main')
+        } else {
+          alert('회원가입 실패')
+        }
+      }).catch((err) => {
+        alert(err)
+      })
+    }
+  }
 }
 </script>
 
