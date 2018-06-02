002bda4a29d441d3027181f9bff8d2ffa0e6dff2
s0j0s <93s0j0s@gmail.com>
2018-06-03 08:25:31 +0900

    이미지파일, 커스텀css 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,13 +12,8 @@ export default {
 
 <style>
 @import url("https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css");
+@import url("http://13.124.202.147:7070/resources/dist/css/AdminLTE.min.css");
 @import url("https://fonts.googleapis.com/css?family=Nanum+Gothic");
 @import url("https://fonts.googleapis.com/earlyaccess/jejugothic.css");
-body {
-  font-family: 'Jeju Gothic', 'Nanum Gothic', 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  background-color: #D2D6DE;
-}
+@import url('assets/custom.css');
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,10 @@ import Router from 'vue-router'
 import Index from '@/components/Index'
 import Login from '@/components/Login'
 import Signup from '@/components/Signup'
+import Diary from '@/components/Diary'
+import Chat from '@/components/Chat'
+import Setting from '@/components/Setting'
+import Project from '@/components/Project'
 
 Vue.use(Router)
 
@@ -28,6 +32,26 @@ export default new Router({
       path: '/signup',
       name: 'Signup',
       component: Signup
+    },
+    {
+      path: '/diary',
+      name: 'Diary',
+      component: Diary
+    },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: Chat
+    },
+    {
+      path: '/setting',
+      name: 'Setting',
+      component: Setting
+    },
+    {
+      path: '/project',
+      name: 'Project',
+      component: Project
     }
   ]
 })
