949e8f7dcfd7c1c2740594deb507d62ba855cd3c
Jinwoo Bae <tkqlzz@gmail.com>
2018-06-07 05:19:50 +0900

    테코용 수정

--- frontend/src/socket/index.js
+++ frontend/src/socket/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import socketio from 'socket.io-client'
 import VueSocketIO from 'vue-socket.io'
 
-export const SocketInstance = socketio('http://localhost:3000') // 'http://localhost:3000'
+export const SocketInstance = socketio() // 'http://localhost:3000'
 
 Vue.use(VueSocketIO, SocketInstance)
 
